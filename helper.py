e94aaa0c65b0f98a9142395da592bf062606e446
Kory <kory@pop-os.localdomain>
2019-03-21 20:49:24 -0500

    Removed unused import from user, added helper.py to ease development.

---
+++ helper.py
@@ -0,0 +1,15 @@
+import os
+from config import Config
+from application import db, create_app
+
+
+def create_database():
+    app = create_app(Config())
+    app_context = app.app_context()
+    app_context.push()
+    db.create_all()
+
+
+if __name__ == '__main__':
+    os.environ.putenv('SECRET_KEY', '123456789')
+    create_database()
